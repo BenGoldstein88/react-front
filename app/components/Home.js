a30ece358336974553dae724079e4a4a7cc11046
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-10-16 12:11:35 -0700

    setup react skeleton for frontend

---
+++ app/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+
+  render() {
+    return (
+      <div>
+      	THIS IS A TEST FOR THE FRONTEND. THIS IS THE HOME COMPONENT.
+      </div>
+    );
+  }
+}
